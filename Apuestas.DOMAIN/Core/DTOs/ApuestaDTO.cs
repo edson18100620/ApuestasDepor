818b5a9056df5194bd96890715910350ae7fa6d0
edson18100620 <81473776+edson18100620@users.noreply.github.com>
2022-06-27 23:38:38 -0500

    Apuesta DTO

---
+++ Apuestas.DOMAIN/Core/DTOs/ApuestaDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApuestasDepor.DOMAIN.Core.DTOs
+{
+    Public  class ApuestaDTO
+    {
+    }
+}
